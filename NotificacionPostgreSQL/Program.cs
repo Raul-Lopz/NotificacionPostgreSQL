cbd622891a1cc8cee3a2784c64c29b9b2938bf5a
José Raúl Lopéz Morales <jraul_lopz@hotmail.com>
2020-09-30 17:46:51 -0500

    Agregar archivos de proyecto.

---
+++ NotificacionPostgreSQL/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using System.Data;
+using System.IO;
+using System.Threading;
+
+namespace NotificacionPostgreSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread HiloS = new Thread(SuscribirseALosCambiosDeLaTablaListado);
+            HiloS.Start();
+            Console.WriteLine("Ahh perro!!!");
+            string entrada = Console.ReadLine();
+        }
+
+        public static void SuscribirseALosCambiosDeLaTablaListado()
+        {
+            const string connString = "host=localhost;Database=db_piscrum;Username=postgres;Password=Learn5503";
+            using (var conn=new NpgsqlConnection(connString))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand("LISTEN datachange", conn))
+                {
+                    cmd.ExecuteNonQuery();
+                    conn.Notification += Personas_Cambio;
+                }
+                while (true)
+                    conn.Wait();
+            }
+
+        }
+
+        public static void Personas_Cambio(object sender, NpgsqlNotificationEventArgs e)
+        {
+            string mensaje = ObtenerMensajeAMostrar(e);
+            Console.WriteLine(mensaje);
+            Console.WriteLine(e.Payload);
+            SuscribirseALosCambiosDeLaTablaListado();
+        }
+
+        public static string ObtenerMensajeAMostrar(NpgsqlNotificationEventArgs e)
+        {
+            string actionName = e.Payload;
+            if (actionName.Contains("DELETE"))
+                return "delete";
+            else if (actionName.Contains("UPDATE"))
+                return "update";
+            else if (actionName.Contains("INSERT"))
+                return "insert";
+            return "";
+        }
+
+
+    }
+}
---
+++ notificaciones.sql
@@ -0,0 +1,59 @@
+/*create a function*/
+-- Schema: public
+create function public."NotifyOnDataChange"()
+RETURNS trigger
+LANGUAGE plpgsql
+AS $BODY$
+DECLARE
+	data JSON;
+	notification JSON;
+BEGIN
+
+IF(TG_OP = 'DELETE') THEN
+	data = row_to_json(OLD);
+ELSE
+	data = row_to_json(NEW);
+END IF;
+
+notification = json_build_object(
+	'table',TG_TABLE_NAME,
+	'action',TG_OP,
+	'data',data);
+
+	PERFORM pg_notify('datachange',notification::TEXT);
+	RETURN NEW;
+END
+$BODY$
+
+/*create de trigger*/
+CREATE TRIGGER "OnDataChange"
+AFTER INSERT OR DELETE OR UPDATE
+ON public."Clasificaciones"
+FOR EACH ROW
+EXECUTE PROCEDURE public."NotifyOnDataChange"();
+
+/*create a function for all tables*/
+CREATE FUNCTION public."CreateOnDataChangeForAllTables"()
+  RETURNS void
+  LANGUAGE 'plpgsql'
+AS $BODY$
+DECLARE  
+  createTriggerStatement TEXT;
+BEGIN
+  FOR createTriggerStatement IN SELECT
+    'CREATE TRIGGER OnDataChange AFTER INSERT OR DELETE OR UPDATE ON '
+    || tab_name
+    || ' FOR EACH ROW EXECUTE PROCEDURE public."NotifyOnDataChange"();' AS trigger_creation_query
+  FROM (
+    SELECT
+      quote_ident(table_schema) || '.' || quote_ident(table_name) as tab_name
+    FROM
+      information_schema.tables
+    WHERE
+      table_schema NOT IN ('pg_catalog', 'information_schema')
+      AND table_schema NOT LIKE 'pg_toast%'
+  ) as TableNames
+  LOOP
+    EXECUTE  createTriggerStatement; --actually create the trigger
+  END LOOP;
+END$BODY$;
